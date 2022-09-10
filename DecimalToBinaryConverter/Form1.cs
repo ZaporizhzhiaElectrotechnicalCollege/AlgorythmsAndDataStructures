9e2a39c79ce116504948af8e20f3daa9e5438bb9
Vladyslav Horbachov <50652041+LeftTwixWand@users.noreply.github.com>
2022-09-10 22:24:26 +0200

    Impemented decimal to binary converter

--- DecimalToBinaryConverter/Form1.cs
+++ DecimalToBinaryConverter/Form1.cs
@@ -1,10 +1,27 @@
-namespace DecimalToBinaryConverter
+﻿namespace DecimalToBinaryConverter;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+    }
+
+    private void ButtonConvert_Click(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(textBoxDecimal.Text))
         {
-            InitializeComponent();
+            MessageBox.Show("Заполните десятическое число!!!");
+            return;
         }
+
+        int.TryParse(textBoxDecimal.Text, out int decimalValue);
+
+        textBoxBinary.Text = NumbersConverter.DecimalToBinary(decimalValue);
+    }
+
+    private void ButtonExit_Click(object sender, EventArgs e)
+    {
+        Close();
     }
 }
---
+++ DecimalToBinaryConverter/NumbersConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace DecimalToBinaryConverter;
+
+internal static class NumbersConverter
+{
+    internal static string DecimalToBinary(int decimalNumber)
+    {
+        if (decimalNumber == 0)
+        {
+            return "0";
+        }
+
+        string binaryNumber = string.Empty;
+
+        // Пока у числа есть остаток - это значит, что мы можем переводить его в двоичное число
+        while (decimalNumber > 0)
+        {
+            // Получаем остаток от деления, что б понимать, что чисто делится на 2 или нет
+            // Если число делится на 2 без остатка - то reminder будет 0. Напрмиер, 8 % 2 = 0
+            // Если число НЕ делится на 2 без остатка - то reminder будет 1. Напрмиер, 9 % 2 = 1
+            int remainder = decimalNumber % 2;
+
+            // Добавляем результат деления в текущую переменную binaryNumber (в начало)
+            // Например, 1 + "101" = "1101"
+            binaryNumber = remainder + binaryNumber;
+
+            // Делим десятичное число на 2 и продолжаем перевод уже с новым значением
+            decimalNumber = decimalNumber / 2;
+        }
+
+        return binaryNumber;
+    }
+}
