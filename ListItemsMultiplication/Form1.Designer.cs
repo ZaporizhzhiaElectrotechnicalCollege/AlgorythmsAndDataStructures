ee16881035d58b23eb9c03ebc56de2b8d67e72fa
Greenfield-Taster <asagorbatchova123@gmail.com>
2022-09-13 23:26:34 +0300

    Added function which fills the list

---
+++ ListItemsMultiplication/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ListItemsMultiplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNumbers = new System.Windows.Forms.TextBox();
+            this.labelText = new System.Windows.Forms.Label();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNumbers
+            // 
+            this.textBoxNumbers.Location = new System.Drawing.Point(222, 28);
+            this.textBoxNumbers.Name = "textBoxNumbers";
+            this.textBoxNumbers.Size = new System.Drawing.Size(127, 20);
+            this.textBoxNumbers.TabIndex = 0;
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelText.Location = new System.Drawing.Point(97, 31);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(108, 13);
+            this.labelText.TabIndex = 1;
+            this.labelText.Text = "Введите 8 значений";
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.BackColor = System.Drawing.Color.Azure;
+            this.buttonResult.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonResult.Location = new System.Drawing.Point(166, 82);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(105, 43);
+            this.buttonResult.TabIndex = 2;
+            this.buttonResult.Text = " Расчитать";
+            this.buttonResult.UseVisualStyleBackColor = false;
+            this.buttonResult.Click += new System.EventHandler(this.ButtonResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(417, 161);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.textBoxNumbers);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Practical Work 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNumbers;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Button buttonResult;
+    }
+}
+
---
+++ ListItemsMultiplication/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ListItemsMultiplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            List<int> listOfNumbers = new List<int>(8);
+
+            // Значения исходные
+            string source = textBoxNumbers.Text;
+
+            for (int index = 0; index < source.Length; index++)
+            {
+                // Текущий символ
+                char currentSymbol = source[index];
+
+                // Проверка на число
+                if (Char.IsDigit(currentSymbol))
+                {
+                    int number;
+
+                    if (int.TryParse(currentSymbol.ToString(), out number))
+                    {
+                        listOfNumbers.Add(number);
+                    }
+                }
+            }
+        }
+    }
+}
