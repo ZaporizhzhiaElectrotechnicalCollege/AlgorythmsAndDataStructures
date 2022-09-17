776bff8b8404f4cc3475392d1aabce325a2ca58a
Greenfield-Taster <asagorbatchova123@gmail.com>
2022-09-17 11:50:14 +0300

    Finished practical work 2

--- ListItemsMultiplication/Form1.Designer.cs
+++ ListItemsMultiplication/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.textBoxNumbers = new System.Windows.Forms.TextBox();
             this.labelText = new System.Windows.Forms.Label();
             this.buttonResult = new System.Windows.Forms.Button();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labelMultiplication = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxNumbers
@@ -54,7 +56,7 @@
             // 
             this.buttonResult.BackColor = System.Drawing.Color.Azure;
             this.buttonResult.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonResult.Location = new System.Drawing.Point(166, 82);
+            this.buttonResult.Location = new System.Drawing.Point(100, 71);
             this.buttonResult.Name = "buttonResult";
             this.buttonResult.Size = new System.Drawing.Size(105, 43);
             this.buttonResult.TabIndex = 2;
@@ -62,12 +64,32 @@
             this.buttonResult.UseVisualStyleBackColor = false;
             this.buttonResult.Click += new System.EventHandler(this.ButtonResult_Click);
             // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(219, 71);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(130, 13);
+            this.labelCount.TabIndex = 3;
+            this.labelCount.Text = "Количество элементов: ";
+            // 
+            // labelMultiplication
+            // 
+            this.labelMultiplication.AutoSize = true;
+            this.labelMultiplication.Location = new System.Drawing.Point(219, 99);
+            this.labelMultiplication.Name = "labelMultiplication";
+            this.labelMultiplication.Size = new System.Drawing.Size(114, 13);
+            this.labelMultiplication.TabIndex = 4;
+            this.labelMultiplication.Text = "Добуток элементов: ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Honeydew;
             this.ClientSize = new System.Drawing.Size(417, 161);
+            this.Controls.Add(this.labelMultiplication);
+            this.Controls.Add(this.labelCount);
             this.Controls.Add(this.buttonResult);
             this.Controls.Add(this.labelText);
             this.Controls.Add(this.textBoxNumbers);
@@ -84,6 +106,8 @@
         private System.Windows.Forms.TextBox textBoxNumbers;
         private System.Windows.Forms.Label labelText;
         private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labelMultiplication;
     }
 }
 
--- ListItemsMultiplication/Form1.cs
+++ ListItemsMultiplication/Form1.cs
@@ -13,10 +13,32 @@ namespace ListItemsMultiplication
 
         private void ButtonResult_Click(object sender, EventArgs e)
         {
-            List<int> listOfNumbers = new List<int>(8);
+            // GetNumbersFromString возвращает список чисел в переменную 
+            List<int> numbers = GetNumbersFromString(textBoxNumbers.Text);
 
-            // Значения исходные
-            string source = textBoxNumbers.Text;
+            int multiplication = 1;
+
+            int count = 0;
+
+            for (int index = 0; index < numbers.Count; index++)
+            {
+                // Добуток на непарних місцях
+                if (index % 2 == 0) // Потому что массив начинается с "0"
+                {
+                    count++;
+                    multiplication *= numbers[index];
+                }
+            }
+
+            labelCount.Text = $"Количество элементов: {count}";
+
+            labelMultiplication.Text = $"Добуток элементов:  {multiplication}";
+        }
+
+        private List<int> GetNumbersFromString(string source)
+        {
+            // Можно добавить размерность 
+            List<int> listOfNumbers = new List<int>();
 
             for (int index = 0; index < source.Length; index++)
             {
@@ -34,6 +56,7 @@ namespace ListItemsMultiplication
                     }
                 }
             }
+            return listOfNumbers;
         }
     }
 }
